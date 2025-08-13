3c33c5cf6bb369fc1a5c1429c92783e6fea76a23
DilanSriyantha <dilans091@gmail.com>
2025-08-13 13:05:49 +0530

    progress eval #1 - minor fixes

--- frontend/app/main/profile.tsx
+++ frontend/app/main/profile.tsx
@@ -8,8 +8,8 @@ export default function Profile() {
     return (
         <View style={styles.container}>
             <View style={styles.userNameContainer}>
-                <Text variant="displayMedium">Dilan Sriyantha</Text>
-                <Avatar.Text size={92} label={auth.currentUser ? auth.currentUser.name.substring(0, 1) : ""} />
+                <Text variant="displayMedium">{auth.currentUser ? auth.currentUser.name : "User"}</Text>
+                <Avatar.Text size={92} label={auth.currentUser ? auth.currentUser.name.substring(0, 1) : "U"} />
             </View>
             <ScrollView style={styles.menu}>
                 <List.Item
